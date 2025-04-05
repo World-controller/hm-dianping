23da393b1e400ee59872ccc64b8ca3a522a9973f
ChenSir <1210309835@qq.com>
2025-04-05 17:24:49 +0800

    商户查询缓存-添加商户缓存

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -14,5 +14,6 @@ import com.hmdp.entity.Shop;
  */
 public interface IShopService extends IService<Shop> {
 
+    Result queryById(Long id);
 
 }
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,11 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.CacheClient;
+import org.springframework.boot.json.GsonJsonParser;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +33,29 @@ import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
-
+    @Override
+    public Result queryById(Long id) {
+//        1.从Redis查询商铺缓存
+        String key = CACHE_SHOP_KEY + id;
+        String shopCache = stringRedisTemplate.opsForValue().get(key);
+//        2.判断是否存在
+        if (StrUtil.isNotBlank(shopCache)){
+            //        3.存在，直接返回商铺信息
+            Shop shop = BeanUtil.toBean(shopCache, Shop.class);
+            return Result.ok(shop);
+        }
+//        4.不存在则根据id查询数据库
+        Shop shop = getById(id);
+//        5.数据库中商铺不存在返回404
+        if(shop == null){
+            return Result.fail("404！商铺不存在！");
+        }
+//        6.如果商铺存在将商铺数据写入Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+//        7.返回商铺信息
+        return Result.ok(shop);
+    }
 }
